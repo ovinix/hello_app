e78bf03e673f91a5d611e680d119dce062af493c
ovinix <dev.shvvolodymyr@hotmail.com>
2016-02-15 20:16:02 +0200

    Exercises. Chapter 1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "Hello, World!"
+  	render text: "¡Hola, mundo!"
+  end
+
+  def goodbye
+  	render text: "Goodbye, World!"
   end
 end
